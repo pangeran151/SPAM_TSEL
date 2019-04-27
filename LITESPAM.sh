9281d8e9f8e30d3036eaa7a3d8853e2a6c152689
PANGERAN GANTENG <49519527+pangeran151@users.noreply.github.com>
2019-04-27 09:03:07 +0700

    Add files via upload

---
+++ LITESPAM.sh
@@ -0,0 +1,77 @@
+#!/system/xbin/bash
+clear
+git clone https://github.com/amsitlab/smsid-java.git
+cd ./smsid-java
+chmod +x ./install
+apt install ./smsid_1.1_all.deb
+clear
+
+toilet -f slant --gay "LITESPAM"
+sleep 1
+echo "\033[33;1m Macam-macam tool untuk spam"
+sleep 1
+echo "\033[36;1m""\033[34;1mPangeran Ganteng""\033[36;1m"
+sleep 1
+echo "\033[32;1m Spam Yang Tersedia :"
+echo "\033[35;1m1.""\033[36;1mTelkomsel"
+echo "\033[31;1m0. Keluar"
+echo "\033[37;1m81.""\033[30;1mInformasi Tool Ini"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+
+if
+[ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[31;1m"
+toilet "T-Sel"
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mDanz"
+sleep 5
+clear
+cd $HOME/LITESPAM/Tul/
+php 1.php
+fi
+
+if
+[ $mrrm = 81 ] || [ $mrrm = 81 ]
+then
+clear
+toilet -f slant --gay "LiteSpam"
+echo "\033[34;1mContact:""\033[37;1m https://fb.me/IrsyadMaulana81"
+
+
+sleep 1
+echo "\033[35;1mPemograman:""\033[36;1m Bash"
+
+echo "\033[33;1mKarya: IM81"
+sleep 1
+echo "\033[32;1mVersi: v3"
+sleep 1
+echo "\033[36;1mTeam: Pangeran Ganteng"
+sleep 1
+echo "\033[31;1mSubscribe YouTube""\033[37;1m Malaikat Maut"
+echo "\033[35;1m Spesial Thanks To: "
+echo "\033[36;1m"
+echo "Pangeran Ganteng"
+echo "Association Indonesian Right Wings"
+sleep 1
+echo "Pangeran Ganteng"
+echo "\033[30;1m tunggu 8 detik"
+sleep 8
+sh litespam.sh
+fi
+
+
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m Sampai berjumpa lagi :)"
+sleep 1
+fi
+
